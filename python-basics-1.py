cb3b1f27a011b0b4fab8b1ad2522847b356d464a
Sadi Cesur <84407005+sadicesur@users.noreply.github.com>
2021-05-19 22:55:59 +0300

    Update python-basics-1.py
    added all solutions

--- python-basics-1.py
+++ python-basics-1.py
@@ -3,11 +3,31 @@
 # S.CESUR
 # =============================================================================
 
-# FLATTEN A GIVEN LIST 
-from itertools import chain
-
+#1 - FLATTEN A GIVEN RANDOM LIST 
 def flatten(listz):
-  return list(chain(*listz))
-
-input_list = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+    result = []
+    if isinstance(listz, (list, tuple)):
+        for i in listz:
+            result.extend(flatten(i))
+    else:
+        result.append(listz)
+    return result
+  
+input_list = [[1,'a',['cat'],2],[[[3]],'dog'],4,5] # any list can be replaced here!
 print(flatten(input_list))
+
+#2 - REVERSE A GIVEN RANDOM LIST
+
+def true_reverse(mylist):
+    result = []
+    for e in mylist:
+        if isinstance(e, list):
+            result.append(true_reverse(e))
+        else:
+            result.append(e)
+    result.reverse()
+    return result
+
+l = [[1, 2], [3, '4'], [5, 6, 7]] # any list can be replaced here!
+print(true_reverse(l))
+
