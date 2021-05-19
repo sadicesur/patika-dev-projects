14675309389acd26a86ed9b5981d899654639f0b
Sadi Cesur <84407005+sadicesur@users.noreply.github.com>
2021-05-19 22:17:05 +0300

    Update python-basics-1.py
    uploaded the solution for the first part

--- python-basics-1.py
+++ python-basics-1.py
@@ -1,3 +1,13 @@
+# =============================================================================
+# PATIKA-DEV PROJECT-1, PYTHON BASICS
+# S.CESUR
+# =============================================================================
+
+# FLATTEN A GIVEN LIST 
 from itertools import chain
-input_list = [[1,2,3],[1,2,3]]
-list(chain(*input_list))
+
+def flatten(listz):
+  return list(chain(*listz))
+
+input_list = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+print(flatten(input_list))
