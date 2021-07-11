f2448692a6d5f43e361e45479c8cd2da31759d36
Sadi Cesur <84407005+sadicesur@users.noreply.github.com>
2021-07-11 11:26:23 +0300

    Update patika-vitra-sql-assignments.sql

--- patika-vitra-sql-assignments.sql
+++ patika-vitra-sql-assignments.sql
@@ -19,3 +19,12 @@ SELECT * FROM film WHERE replacement_cost BETWEEN 12.99 AND 16.99;
 SELECT first_name, last_name FROM actor WHERE first_name IN ('Penelope', 'Nick', 'Ed');
 -- Question 3
 SELECT * FROM film WHERE rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN (12.99, 15.99, 28.99);
+
+-- SQL ASSIGNMENT #3
+
+-- Question 1
+SELECT country FROM country WHERE country LIKE 'A%a';
+-- Question 2
+SELECT country FROM country WHERE LENGTH(country) > 6 and country LIKE '%n';
+-- Question 3
+SELECT title FROM film WHERE title ILIKE '%t%t%t%t%';
