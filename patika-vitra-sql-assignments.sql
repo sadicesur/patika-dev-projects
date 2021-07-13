f0fae65384c28f7b5b60697383255e99afa679b6
Sadi Cesur <84407005+sadicesur@users.noreply.github.com>
2021-07-13 19:39:15 +0300

    Update patika-vitra-sql-assignments.sql

--- patika-vitra-sql-assignments.sql
+++ patika-vitra-sql-assignments.sql
@@ -50,3 +50,15 @@ SELECT * FROM film WHERE title LIKE '%n'ORDER BY length DESC LIMIT 5;
 SELECT * FROM film WHERE title LIKE '%n'ORDER BY length ASC LIMIT 5 OFFSET 2;
 -- Question 3
 SELECT * FROM customer WHERE store_id = 1 ORDER BY last_name DESC LIMIT 4;
+
+
+-- SQL ASSIGNMENT #6
+
+-- Question 1
+SELECT AVG(rental_rate) FROM film;
+-- Question 2
+SELECT COUNT (*) FROM film WHERE title LIKE 'C%';
+-- Question 3
+SELECT MAX(length) FROM film WHERE rental_rate = 0.99;
+-- Question 4
+SELECT COUNT (DISTINCT replacement_cost) FROM film WHERE length > 150;
