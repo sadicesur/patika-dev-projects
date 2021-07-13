9db1011acbd478a9888fdc0ebca49d86dbc2ea74
Sadi Cesur <84407005+sadicesur@users.noreply.github.com>
2021-07-13 19:12:59 +0300

    Update patika-vitra-sql-assignments.sql

--- patika-vitra-sql-assignments.sql
+++ patika-vitra-sql-assignments.sql
@@ -41,3 +41,12 @@ SELECT COUNT (*) FROM film WHERE title LIKE 'T%' AND rating = 'G';
 SELECT COUNT (*) FROM country WHERE LENGTH(country)  = 5;
 -- Question 5
 SELECT COUNT (*) FROM city WHERE city ILIKE '%R'
+
+-- SQL ASSIGNMENT #5
+
+-- Question 1
+SELECT * FROM film WHERE title LIKE '%n'ORDER BY length DESC LIMIT 5;
+-- Question 2
+SELECT * FROM film WHERE title LIKE '%n'ORDER BY length ASC LIMIT 5 OFFSET 2;
+-- Question 3
+SELECT * FROM customer WHERE store_id = 1 ORDER BY last_name DESC LIMIT 4;
