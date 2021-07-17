c4db157dda308b33d4f0c450712de6b4ca4217e2
Sadi Cesur <84407005+sadicesur@users.noreply.github.com>
2021-07-17 16:24:55 +0300

    Update patika-vitra-sql-assignments.sql

--- patika-vitra-sql-assignments.sql
+++ patika-vitra-sql-assignments.sql
@@ -62,3 +62,15 @@ SELECT COUNT (*) FROM film WHERE title LIKE 'C%';
 SELECT MAX(length) FROM film WHERE rental_rate = 0.99;
 -- Question 4
 SELECT COUNT (DISTINCT replacement_cost) FROM film WHERE length > 150;
+
+-- SQL ASSIGNMENT #7
+
+-- Question 1
+SELECT rating FROM film GROUP BY rating;
+-- Question 2
+SELECT replacement_cost,COUNT(*) FROM film GROUP BY replacement_cost HAVING COUNT(*) > 50;
+-- Question 3
+SELECT store_id,COUNT(*) FROM customer GROUP BY store_id;
+-- Question 4
+SELECT country_id,COUNT(*) FROM city GROUP BY country_id ORDER BY COUNT(*) DESC LIMIT 1;
+
