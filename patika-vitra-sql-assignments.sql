ae7c1cbe43bcfef8b460aaa64af60d441731c1e9
Sadi Cesur <84407005+sadicesur@users.noreply.github.com>
2021-07-17 16:33:57 +0300

    Update patika-vitra-sql-assignments.sql

--- patika-vitra-sql-assignments.sql
+++ patika-vitra-sql-assignments.sql
@@ -74,3 +74,71 @@ SELECT store_id,COUNT(*) FROM customer GROUP BY store_id;
 -- Question 4
 SELECT country_id,COUNT(*) FROM city GROUP BY country_id ORDER BY COUNT(*) DESC LIMIT 1;
 
+-- SQL ASSIGNMENT #8
+
+-- Question 1
+CREATE TABLE employee(id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL, email VARCHAR(100), birthday DATE);
+-- Question 2
+
+-- Question 3
+UPDATE Employee SET name = 'Barbaros' WHERE id = 1;
+UPDATE Employee SET email = 'rtb.barbaros@gmail.com' WHERE name = 'Barbaros';
+UPDATE Employee SET birthday = '2021/05/29' WHERE email = '123@gmail.com';
+UPDATE Employee SET birthday = '2021/05/29',name = 'Revaha' WHERE email = '123@gmail.com';
+UPDATE Employee SET name = 'Barbaros',birthday = '2021/05/29' WHERE id = 1;
+-- Question 4
+DELETE FROM Employee WHERE name = 'Revaha';
+DELETE FROM Employee WHERE email = 'rtb.barbaros@gmail.com';
+DELETE FROM Employee WHERE birthday = '2021/05/29';
+DELETE FROM Employee WHERE name = 'Tayyip';
+DELETE FROM Employee WHERE id = '1';
+
+-- SQL ASSIGNMENT #9
+
+-- Question 1
+SELECT city,country FROM city ci JOIN country co ON (co.country_id = ci.country_id );
+-- Question 2
+SELECT first_name,last_name,payment_id FROM customer c JOIN payment p ON ( p.customer_id = c.customer_id);
+-- Question 3
+SELECT first_name,last_name,rental_id FROM customer c JOIN rental r ON( c.customer_id = r.customer_id );
+
+-- SQL ASSIGNMENT #10
+
+-- Question 1
+SELECT city,country FROM city ci LEFT JOIN country co ON ( ci.country_id = co.country_id );
+-- Question 2
+SELECT first_name,last_name,payment_id FROM customer c RIGHT JOIN payment p ON ( p.customer_id = c.customer_id);
+-- Question 3
+SELECT first_name,last_name,rental_id FROM customer c FULL JOIN rental r ON( c.customer_id = r.customer_id );
+
+
+-- SQL ASSIGNMENT #11
+
+-- Question 1
+( SELECT first_name FROM actor )
+UNION
+( SELECT first_name FROM customer )
+-- Question 2
+( SELECT first_name FROM actor )
+INTERSECT
+( SELECT first_name FROM customer )
+-- Question 3
+( SELECT first_name FROM actor )
+EXCEPT
+( SELECT first_name FROM customer )
+-- Question 4
+( SELECT first_name FROM actor )
+UNION ALL
+( SELECT first_name FROM customer )
+( SELECT first_name FROM actor )
+INTERSECT ALL
+( SELECT first_name FROM customer )
+( SELECT first_name FROM actor )
+EXCEPT ALL
+( SELECT first_name FROM customer )
+
+
+
+
+
+
